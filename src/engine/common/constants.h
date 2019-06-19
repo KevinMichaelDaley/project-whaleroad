8f49b6d44cd33302f5079379ea99855953dc6d94
Kevin Daley <kdaley3@gsu.edu>
2019-06-19 11:24:04 -0400

    fixed light propagation + ordering via zorder

--- src/engine/common/constants.h
+++ src/engine/common/constants.h
@@ -8,7 +8,7 @@ constexpr const  int MAX_DATAGRAM_SIZE =
     UDP_MTU - MAX_IP_HEADER_SIZE - UDP_HEADER_SIZE;
 constexpr const  int CHUNK_WIDTH = 16;
 constexpr const  int MAX_RESIDENT_PAGES=90;
-constexpr const  int PAGE_DIM=96;       
+constexpr const  int PAGE_DIM=128;       
 constexpr const  int LIGHT_COMPONENTS=12;    
 constexpr const int INVERSE_COMPONENTS[]={1,0,3,2,5,4};
 constexpr const  float LPV_WEIGHT[]={
